f4e4fd215ea2bb12f16e73cc60e240e1a19f090a
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-04-11 17:31:50 -0300

    iniciando as classes

---
+++ src/creature.ts
@@ -0,0 +1,16 @@
+abstract class Creature {
+    public readonly name: string
+    public readonly heatlh: number
+    public readonly attackPower: number
+    
+    
+    constructor(name: string, heatlh: number, attackPower: number) {
+        this.name = name
+        this.heatlh = heatlh
+        this.attackPower = attackPower
+    }
+
+    public abstract move(): string
+    public abstract attack(target: Creature): string
+    public abstract defend(amount: number): void
+}
